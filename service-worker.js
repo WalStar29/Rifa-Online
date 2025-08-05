bd255be68446533a9788058415299975b8031ce5
WalStar29 <castrowalter172913@hotmail.com>
2025-08-05 15:22:34 -0400

    nuevo

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,40 @@
+const CACHE_NAME = 'mega-rifa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.jpg',
+  './manifest.json'
+];
+
+// Instalar y cachear archivos
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// Activar y limpiar cachés antiguas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Interceptar peticiones y responder desde caché si es posible
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
 });
